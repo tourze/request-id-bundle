5452d9fa5bafeb5db20a08eb7d0562a1c34f3e39
tourze-ci <you@example.com>
2025-04-10 05:14:26 +0000

    refactor: 移除 RequestIdBundle 的 BundleDependencyInterface 实现

--- src/RequestIdBundle.php
+++ src/RequestIdBundle.php
@@ -5,17 +5,9 @@ namespace RequestIdBundle;
 use RequestIdBundle\Middleware\RequestIdMiddleware;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 use Tourze\BacktraceHelper\Backtrace;
-use Tourze\BundleDependency\BundleDependencyInterface;
 
-class RequestIdBundle extends Bundle implements BundleDependencyInterface
+class RequestIdBundle extends Bundle
 {
-    public static function getBundleDependencies(): array
-    {
-        return [
-            \AopCoroutineBundle\AopCoroutineBundle::class => ['all' => true],
-        ];
-    }
-
     public function boot(): void
     {
         parent::boot();
